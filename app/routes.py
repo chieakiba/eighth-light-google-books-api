7bf867a84be8624b6803cd71f23d4f76209c3465
Chie Akiba <akiba.chie@gmail.com>
2019-01-27 21:17:27 -0800

    Updated Readme for more information. Added google books api search function that enables user to search for a book title and it will return a list with author, title, publisher and image. Added bootstrap to add simple styles to the app. Updated routes to make GET call to retrieve list of books

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,24 @@
-from flask import render_template
+import requests
+import json
+from flask import render_template, request
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
+
+@app.route('/lookup', methods=['GET'])
+def lookUpBook():
+    try:
+        if request.method == "GET":
+            user_search = request.values.get('usersearch')
+            r = requests.get('https://www.googleapis.com/books/v1/volumes?q='+user_search)
+            return render_template('index.html', content=json.loads(r.text))
+
+    except Exception as error:
+        return render_template('index.html', error=error)
+
 
 if __name__ == '__main__':
     app.run()
